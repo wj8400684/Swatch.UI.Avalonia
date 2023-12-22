ccb0aa0f6f9778c833cc923f5ce74df7013394b2
mockingjay <8400684@qq.com>
2023-12-23 02:42:47 +0800

    ini

--- demo/AvaloniaNavigation/Views/MainWindow.axaml.cs
+++ demo/AvaloniaNavigation/Views/MainWindow.axaml.cs
@@ -1,12 +1,11 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
-using SwatchAvalonia.AppWindow.Controls;
 using SwatchAvalonia.Navigation.Controls;
 
 namespace AvaloniaNavigation.Views;
 
-public partial class MainWindow : AppWindow
+public partial class MainWindow : Window
 {
     public MainWindow()
     {
