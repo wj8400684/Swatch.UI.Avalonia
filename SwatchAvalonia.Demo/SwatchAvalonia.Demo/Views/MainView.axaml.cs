b54539fa3d55406a2e1e45e25876b0b0102fbb9f
mockingjay <mockingjay@MOCKINGJAYBE2D>
2023-12-24 11:09:38 +0800

    ini

---
+++ SwatchAvalonia.Demo/SwatchAvalonia.Demo/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SwatchAvalonia.Demo.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SwatchAvalonia.Demo/SwatchAvalonia.Demo/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SwatchAvalonia.Demo.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
